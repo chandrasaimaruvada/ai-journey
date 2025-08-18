0cff625427d0313157edbb7a019d63bf05e1289b
chandrasaimaruvada <chandrasaimaruvada@gmail.com>
2025-08-18 17:03:56 +0530

    Day 2: Python variables and data types

---
+++ projects/day2_basics.py
@@ -0,0 +1,21 @@
+# Day 2 - Variables and Data Types
+
+# Store personal info
+name = "chandra Sai"
+age = 25
+fav_language = "Python"
+
+# Print with f-string
+print(f"My name is {name}, I am {age} years old, and I love {fav_language}.")
+
+# List of AI fields
+ai_fields = ["NLP", "Computer Vision", "Reinforcement Learning"]
+print("AI fields I'm excited about:", ai_fields)
+
+# Dictionary of today’s info
+learning_log = {
+    "name": name,
+    "goal": "Become a Pro AI Engineer",
+    "day": 2
+}
+print("Learning Log:", learning_log)
